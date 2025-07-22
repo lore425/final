0a7d75781498fa09f4c6f3599777687a1d6216e5
desarrollo <desarrollo@academicos.cesde.edu.co>
2025-07-22 14:59:56 -0500

    ok

--- analisis.py
+++ analisis.py
@@ -3,18 +3,21 @@ import pandas as pd
 
 #cargar los datos a analizar 
 datosEmpleados=pd.read_csv("datos_empleados_bar.csv")
-print(datosEmpleados)
+
 
 #aplicando tranformaciones y filtros
 #clasificar los empleados con salario a tre millones
 salariosMayoresATres=datosEmpleados.query("`Salario (COP)`>3000000")
-
+print(salariosMayoresATres.head(5))
 #obtener los empleados con mas de 10 años de experienciA
 empleadosExperienciaDiez=datosEmpleados.query("`Experiencia (años)`>10")
+print(empleadosExperienciaDiez.head(5))
 
 #empleados que trabajan mas de 180 horas
 empleados180=datosEmpleados.query("`Horas laboradas/mes`>180")
+print(empleados180.head(5))
 
 
  #necesito optener empleados cuyo cargo es bartender
-bartenders=datosEmpleados.query("`Cargo en el bar`==`Bartender`") 
+bartenders=datosEmpleados.query("`Cargo en el bar`=='Bartender'") 
+print(bartenders.head(5))
